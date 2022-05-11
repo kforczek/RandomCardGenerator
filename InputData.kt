a7cf67857bebbebb8810315721c71a3e65bebcd9
Kuba Sławomir Forczek <01158632@pw.edu.pl>
2022-05-11 20:23:34 +0200

    Add documentation

--- InputData.kt
+++ InputData.kt
@@ -1,3 +1,8 @@
+/* Represents user's choice of the deck parameters.
+*  The deck will contain cards with values from
+*  cardValMin (inclusive) to cardValMax (inclusive),
+*  in suits defined by cardSuits and in colors defined
+*  by cardColors. */
 data class InputData(val cardValMin: CardValue = CardValue.ACE,
                      val cardValMax: CardValue = CardValue.JOKER,
                      val cardSuits: Set<CardSuit> = CardSuit.values().toSet(),
