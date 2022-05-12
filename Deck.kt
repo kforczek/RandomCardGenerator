c9866c496807c11d579aad25467795d0cd381b60
Kuba Sławomir Forczek <01158632@pw.edu.pl>
2022-05-12 18:40:45 +0200

    Fix Deck implementation

--- Deck.kt
+++ Deck.kt
@@ -12,8 +12,8 @@ class Deck(deckParam: InputData) {
             if (cardVal < deckParam.cardValMin || cardVal > deckParam.cardValMax)
                 continue
 
-            for (cardSuit in CardSuit.values()) {
-                for (cardCol in CardColor.values()) {
+            for (cardSuit in deckParam.cardSuits) {
+                for (cardCol in deckParam.cardColors) {
                     cards.add(Card(cardVal, cardSuit, cardCol))
                 }
             }
@@ -22,6 +22,9 @@ class Deck(deckParam: InputData) {
 
     fun nextCard(): Card
     {
+        if (this.isEmpty())
+            throw IllegalStateException("There are no cards left in the deck.")
+
         val card = cards[Random.nextInt(cards.size)]
         cards.remove(card)
         return card
