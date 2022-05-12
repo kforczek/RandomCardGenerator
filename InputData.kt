4130e446812b00ad2082bd4012d1e931e0baa894
Kuba Sławomir Forczek <01158632@pw.edu.pl>
2022-05-12 18:42:12 +0200

    Generalize InputData constructor

--- InputData.kt
+++ InputData.kt
@@ -3,7 +3,7 @@
 *  cardValMin (inclusive) to cardValMax (inclusive),
 *  in suits defined by cardSuits and in colors defined
 *  by cardColors. */
-data class InputData(val cardValMin: CardValue = CardValue.ACE,
-                     val cardValMax: CardValue = CardValue.JOKER,
+data class InputData(val cardValMin: CardValue = CardValue.values().first(),
+                     val cardValMax: CardValue = CardValue.values().last(),
                      val cardSuits: Set<CardSuit> = CardSuit.values().toSet(),
                      val cardColors: Set<CardColor> = CardColor.values().toSet())
