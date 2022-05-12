b6249b27b0e916244fffbe0883a3edb0414967db
Kuba Sławomir Forczek <01158632@pw.edu.pl>
2022-05-12 18:44:59 +0200

    Main entry point

--- RandomCardGenerator.kt
+++ RandomCardGenerator.kt
@@ -2,7 +2,14 @@ fun main(args: Array<String>)
 {
     if (args.contains("-nogui"))
     {
-
+        val gen = Generator()
+        val ui = TextUserInterface(gen)
+        gen.setUI(ui)
+        gen.run()
     }
-
+    else
+    {
+        println("Graphical UI not implemented yet." +
+                " Please use the text UI by running with \"-nogui\".")
+    }
 }
